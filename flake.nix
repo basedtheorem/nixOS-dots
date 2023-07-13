4ee843c2d1f2eb00526bb8107663905334a52b64
basedtheorem <lrns@proton.me>
2023-07-13 17:49:34 +1200

    perf: additional binary caches; refactor: output func. signature

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
     parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { parts, ... } @inputs:
-    parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+
+  outputs = inputs:
+    inputs.parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
       imports = [
         ./parts/home_configs.nix 
@@ -21,10 +22,20 @@
       };
   };
 
+
   nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://helix.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://cache.privatevoid.net"
+    ];
+
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
     ];
   };
 }
