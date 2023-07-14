97a87259e97eca82abd1e377d69c275fcaf1ca0e
basedtheorem <lrns@proton.me>
2023-07-15 08:18:12 +1200

    fix: remove suffix, add comments

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,15 +1,20 @@
 { lib, ... }:
-
+/*
+Imports every module in the current directory
+and assigns them to their file names.
+*/
 let
+  # Filter for `*.nix` files.
   modules = lib.filterAttrs (
     name: val:
-      if val == "regular" then (
-        if name != "default.nix" then true
-        else false
-      )
+      # "regular" means normal file as opposed to (e.g.) a directory.
+      if (val == "regular" && name != "default.nix") then true
       else false
   ) (builtins.readDir ./.);
 
 in {
-  builtins.mapAttrs (name: _: import (./. + "/${name}")) modules;
+  lib.mapAttrs (name: val: lib.nameValuePair
+    (lib.removeSuffix ".nix" name) # "module.nix" => "module"
+    (import (./. + "/${name}"))
+  ) modules;
 }
